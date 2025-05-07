342ad46de227fd703e2d5425a33b5e8bb9c98df7
A Completely Normal Popcat <122589310+Synapsian@users.noreply.github.com>
2025-05-07 09:55:14 -0700

    Reworked NPCs

--- resources/npc_resource.gd
+++ resources/npc_resource.gd
@@ -1,15 +1,41 @@
-class_name NPC
 extends Sprite2D
 
-@export var NPCDialogue : String
+@export var DialogueText : String
+@export var DialogueSpeed : int
+
 @onready var Hitbox = $Area2D
+@onready var Arrow = $Arrow
 
-func _area_entered():
-	print("Entered")
+var PlayerInside = false
+var Debounce = false
 
-func _area_exited():
-	print("Area Exited")
+func _body_entered(body):
+	if not body.is_in_group("Player"): return
+	PlayerInside = true
+	
+	Arrow.modulate.a = 0
+	Arrow.visible = true
+	var tween = get_tree().create_tween()
+	tween.tween_property(Arrow,"modulate:a",1,0.5)
+	
+
+func _body_exited(body):
+	if not body.is_in_group("Player"): return
+	PlayerInside = false
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property(Arrow,"modulate:a",0,0.5)
 
 func _ready() -> void:
-	Hitbox.connect("area_entered",_area_entered)
-	
+	Hitbox.connect("body_entered",_body_entered)
+	Hitbox.connect("body_exited",_body_exited)
+
+func _process(_delta: float) -> void:
+	if PlayerInside and Input.is_action_just_pressed("Interact"):
+		if Debounce: return
+		Debounce = true
+		
+		Dialogue.start_dialogue(DialogueText,DialogueSpeed,texture)
+		
+		await  Dialogue.dialogue_finished
+		Debounce = false
