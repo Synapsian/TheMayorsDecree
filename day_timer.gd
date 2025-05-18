97b0ac4b8cbddbd2e5dfc4394cd5aa84c7f17c94
A Completely Normal Popcat <122589310+Synapsian@users.noreply.github.com>
2025-05-18 14:41:45 -0700

    Forgot to change start time

--- day_timer.gd
+++ day_timer.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
-@export var start_time:int = 100
+@export var start_time:int = 80
 @export var enabled:bool = false
 @onready var label:Label = $time_remaining
 
 signal finished
-var current_time_left = 20
+var current_time_left = 80
 var game_finished = false
 
 func _process(delta: float) -> void:
