782b38a102e2f6a0d56a7ce24d03b7346ebb8b3b
A Completely Normal Popcat <122589310+Synapsian@users.noreply.github.com>
2025-05-19 10:23:29 -0700

    Fishing Minigame 0.1

--- SCENES/fishing_minigame.gd
+++ SCENES/fishing_minigame.gd
@@ -1,6 +1,18 @@
 extends CanvasLayer
 
-var fish = preload('res://fish.tscn')
+@onready var fish = preload('res://fish.tscn')
+@onready var spawn_location = $fish_spawnpoints/fish_spawnlocation
+
+var fishies = []
+
+func _ready() -> void:
+	add_new_fish()
 
 func add_new_fish():
 	var new_fish = fish.instantiate()
+	spawn_location.progress_ratio = randf()
+	new_fish.position = spawn_location.position
+	new_fish.visible = true
+	fishies.append(new_fish)
+	new_fish.add_to_group("Fish")
+	add_child(new_fish)
