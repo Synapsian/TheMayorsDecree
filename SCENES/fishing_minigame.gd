bc35028b816e82cd0b29390a127836b543234dfd
A Completely Normal Popcat <122589310+Synapsian@users.noreply.github.com>
2025-05-22 09:00:34 -0700

    Fishing Minigame 0.6

--- SCENES/fishing_minigame.gd
+++ SCENES/fishing_minigame.gd
@@ -5,21 +5,35 @@ extends CanvasLayer
 @onready var midpoint = $midpoint
 @onready var bounding_boxes = $bounding_boxes
 @onready var crosshair = $crosshair
+@onready var midpoint_spawn = $midpoint_randomizer/midpoint_path_follow
 
 var points = 0
 
+func destroy_timer(timer):
+	timer.queue_free()
+
+func wait(time:float):
+	var new_timer = Timer.new()
+	new_timer.one_shot = true
+	new_timer.wait_time = time
+	add_child(new_timer)
+	new_timer.start()
+	new_timer.timeout.connect(destroy_timer.bind(new_timer))
+	return new_timer.timeout
+
 func _ready() -> void:
-	
 	for bounding_box in bounding_boxes.get_children():
 		bounding_box.body_entered.connect(_bounding_box_entered)
-
-	add_new_fish()
+	start_minigame()
 
 func _bounding_box_entered(body:Node2D):
 	if body.is_in_group("Fish"):
 		body.queue_free()
 
 func add_new_fish():
+	midpoint_spawn.progress_ratio = randf()
+	midpoint.position = midpoint_spawn.position
+	
 	var new_fish = fish.instantiate()
 	spawn_location.progress_ratio = randf()
 	new_fish.position = spawn_location.position
@@ -34,9 +48,9 @@ func add_points(amount:int):
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		shoot()
+		_shoot()
 
-func shoot():
+func _shoot():
 	var mouse_position = get_viewport().get_mouse_position()
 	mouse_position = Vector2(clamp(mouse_position.x,375,775),clamp(mouse_position.y,120,520))
 	crosshair.position = mouse_position
@@ -45,3 +59,9 @@ func shoot():
 			if body.is_in_group("Fish"):
 				body.remove_fish()
 				return
+
+func start_minigame():
+	for i in range(1,100):
+		add_new_fish()
+		if randi_range(1,2) == 2:
+			await wait(randf())
